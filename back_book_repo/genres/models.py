acec97026c6606fd0e4c498ff6600d3387ea8eb5
balaclava <nebotxavi@gmail.com>
2019-12-30 08:36:14 +0100

    Correct permissions and unique genres

--- back_book_repo/genres/models.py
+++ back_book_repo/genres/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=150, blank=False)
+    name = models.CharField(max_length=150, blank=False, unique=True)
 
     def __str__(self):
         return self.name
